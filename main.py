5aaf94ff33170b19dfcb335e21e4ca3dc79e9087
maddy <mudassirhussain924@gmail.com>
2026-03-18 00:41:54 +0530

    main file added

---
+++ main.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init
+
+def speak(text):
+    engine
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("How are you")
